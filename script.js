a23025f07b2e8e780c367fe98137202674728bd6
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2025-08-29 22:51:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+//your JS code here. If required.https://www.svgrepo.com/show/345221/three-dots.svg
+const modal = document.querySelector('.modal');
+const openBtn = document.getElementById('openModal');
+const closeBtn = document.querySelector('.close-modal');
+
+openBtn.onclick = () => {
+  modal.style.display = 'flex';
+};
+
+closeBtn.onclick = () => {
+  modal.style.display = 'none';
+};
+
+window.onclick = (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+};
